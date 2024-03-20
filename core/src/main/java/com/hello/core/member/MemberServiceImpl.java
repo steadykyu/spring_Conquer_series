6e660fbfc427c1e7692b1b05f25fcbd246f8b254
steadykyu <awakekyu1@gmail.com>
2024-03-20 20:18:33 +0900

    컴포넌트 스캔 기본

--- core/src/main/java/com/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/com/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,12 @@
 package com.hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
+    @Autowired
     private final MemberRepository memberRepository;
 
     public MemberServiceImpl(MemberRepository memberRepository) {
--- core/src/main/java/com/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/com/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package com.hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
     // 회원을 저장할 내부 메모리
     private static Map<Long, Member> store = new HashMap<>();
